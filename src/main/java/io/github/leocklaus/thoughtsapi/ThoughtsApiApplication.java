3a3455565da51b3ad7561bbc424abc5fcac4f5b5
leocklaus <leocklaus@gmail.com>
2023-12-08 14:35:37 -0300

    feat: project created

---
+++ src/main/java/io/github/leocklaus/thoughtsapi/ThoughtsApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.leocklaus.thoughtsapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThoughtsApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThoughtsApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/leocklaus/thoughtsapi/ThoughtsApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.leocklaus.thoughtsapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThoughtsApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
