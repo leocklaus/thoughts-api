e8522ac842b096ca80fecbcac5c8bae6b8523b06
leocklaus <leocklaus@gmail.com>
2024-02-15 08:52:43 -0300

    fix: now it handles not authorized custom exception

--- src/main/java/io/github/leocklaus/thoughtsapi/api/exceptionhandler/ExceptionType.java
+++ src/main/java/io/github/leocklaus/thoughtsapi/api/exceptionhandler/ExceptionType.java
@@ -9,7 +9,8 @@ public enum ExceptionType {
     THOUGHT_NOT_FOUND("Thought não encontrado", "/thought-not-found"),
     INVALID_DATA("Dados inválidos", "/invalid-data"),
     RESOURCE_NOT_FOUND("Recurso não encontrado", "/resource-not-found"),
-    WRONG_PASSWORD("A senha digitada é incorreta", "/wrong-password");
+    WRONG_PASSWORD("A senha digitada é incorreta", "/wrong-password"),
+    NOT_AUTHORIZED("Requisição não autorizada ou credenciais inválidas", "/not-authorized");
 
     private String title;
     private String URI;
